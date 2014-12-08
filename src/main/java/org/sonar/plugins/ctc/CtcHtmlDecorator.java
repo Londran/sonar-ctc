9bef807d4e02db18367f4134a721b9fa7dfe8088
Sebastian Goetzinger <bastiangoe@hotmail.com>
2014-12-08 14:43:27 +0100

    HTML hinzugefügt

---
+++ src/main/java/org/sonar/plugins/ctc/CtcHtmlDecorator.java
@@ -0,0 +1,113 @@
+/*
+ * Testwell CTC++ Plugin
+ * Copyright (C) 2014 Verifysoft Technology GmbH
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.ctc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.Source;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.batch.Decorator;
+import org.sonar.api.batch.DecoratorContext;
+import org.sonar.api.measures.Measure;
+import org.sonar.api.resources.Project;
+import org.sonar.api.resources.Resource;
+import org.sonar.plugins.ctc.api.measures.CtcMetrics;
+
+public class CtcHtmlDecorator implements Decorator {
+	
+	private static final String[] indizes = {"CTCHTML/indexF.html", "CTCHTML/indexC.html"};
+	
+	private final static Pattern FILE_PATTERN = Pattern.compile("\\./?([^\\.]*(\\..+)?)");
+
+	private Map<String, String> hrefMap;
+	public static final Logger log = LoggerFactory
+			.getLogger(CtcHtmlDecorator.class);
+
+	public CtcHtmlDecorator() {
+		log.info("CTCHTML_DECORATOR STARTED");
+		hrefMap = new HashMap<String, String>();
+		try {
+			for (String s : indizes) {
+				parseSource(new File(s));
+			}
+
+			if (log.isDebugEnabled()) {
+				log.debug("Map");
+				for (Entry<String, String> entry : hrefMap.entrySet()) {
+					log.debug("Map - Entry: '{}':'{}'",entry.getKey(),entry.getValue());
+				}
+			}
+		} catch (IOException e) {
+			hrefMap = null;
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+	private void parseSource (File file) throws IOException {
+		Matcher matcher = FILE_PATTERN.matcher("");
+		Source source = new Source(file);
+		log.debug("Useing source {}", source);
+		for (Element element : source.getAllElements("a")) {
+			matcher.reset(element.getTextExtractor().toString());
+			if (matcher.matches()) {
+				String path;
+				if (matcher.group(2) != null) {
+					path = matcher.group(1);
+				} else {
+					path = matcher.group(1) + "/";
+				}
+				log.debug("Adding '{}' --> '{}'",path, element.getAttributeValue("href"));
+				hrefMap.put(path, element.getAttributeValue("href"));
+			}
+		}
+	}
+
+	@Override
+	public boolean shouldExecuteOnProject(Project project) {
+		return hrefMap != null;
+	}
+
+	@Override
+	public void decorate(Resource resource, DecoratorContext context) {
+		
+		String href = hrefMap.get(resource.getPath());
+		if (href == null) {
+			href = "index.html";
+		}
+		log.debug("Mapping orig report {} --> {}", resource, href);
+		@SuppressWarnings("rawtypes")
+		Measure path = new Measure(CtcMetrics.CTC_ORIG_REPORT_NAME);
+		path.setData(href);
+		context.saveMeasure(path);
+		
+	}
+
+}
---
+++ src/main/java/org/sonar/plugins/ctc/widgets/CtcTreemapWidget.java
@@ -0,0 +1,61 @@
+/*
+ * Testwell CTC++ Plugin
+ * Copyright (C) 2014 Verifysoft Technology GmbH
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.ctc.widgets;
+
+import org.sonar.api.web.AbstractRubyTemplate;
+import org.sonar.api.web.RubyRailsWidget;
+import org.sonar.api.web.WidgetProperties;
+import org.sonar.api.web.WidgetProperty;
+import org.sonar.api.web.WidgetPropertyType;
+import org.sonar.plugins.ctc.api.measures.CtcMetrics;
+
+@WidgetProperties({
+	  @WidgetProperty(key = "chartTitle", type = WidgetPropertyType.STRING),
+	  @WidgetProperty(key = "sizeMetric", type = WidgetPropertyType.METRIC, defaultValue = CtcMetrics.CTC_STATEMENTS_TO_COVER_KEY, options = {"domain:Tests \\(Testwell CTC\\+\\+\\)"}),
+	  @WidgetProperty(key = "colorMetric", type = WidgetPropertyType.METRIC, defaultValue = CtcMetrics.CTC_CONDITION_COVERAGE_KEY,
+	    options = {"domain:Tests \\(Testwell CTC\\+\\+\\)", "type:PERCENT"}),
+	  @WidgetProperty(key = "heightInPercents", type = WidgetPropertyType.INTEGER, optional = true, defaultValue = "55"),
+	  @WidgetProperty(key = "maxItems", type = WidgetPropertyType.INTEGER, defaultValue = "100"),
+	  @WidgetProperty(key = "reportBaseUrl", type = WidgetPropertyType.STRING)
+	})
+
+public class CtcTreemapWidget extends AbstractRubyTemplate implements
+		RubyRailsWidget {
+
+	public CtcTreemapWidget() {
+		
+	}
+
+	@Override
+	public String getId() {
+		return "ctc_treemap_widget";
+	}
+
+	@Override
+	public String getTitle() {
+		return "Testwell CTC++ Treemap of Components";
+	}
+
+	@Override
+	protected String getTemplatePath() {
+		return "/org/sonar/plugins/ctc/widgets/ctc_treemap.html.erb";
+	}
+
+}
